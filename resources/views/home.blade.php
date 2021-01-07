580e88b3d9a9cd9d7be6e46ee7b7d8bacf89f593
Antonia P <direiellie@gmail.com>
2021-01-07 23:29:54 +0100

    Laravel Comics (in intinere)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('content')
+<main class="wrap-home">
+    <section class="general-hero">
+        <div class="container">
+            <img src="{{ asset ('images/cover-home.jpg')  }}" alt="Teen Titans Go">
+        </div>
+    </section>
+    <!--Comics Section-->
+    <section class="comics-section">
+        <div class="container">
+            LISTA COMICS
+        </div>
+
+    </section>
+
+
+</main>
+    
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,52 @@
+{{--
+    MAIN SITE HEADER
+    --}}
+
+    <header class="main-header">
+        <div class="container">
+            <!--Utilizzo slash perchè punto alla HP-->
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset ('images/logo.png') }}" alt="DC Comics logo">
+            </a>
+
+            <nav class="navbar-nav">
+                <ul>
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="#">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+
+                </ul>
+
+            </nav>
+            
+
+        </div>
+
+    </header>
